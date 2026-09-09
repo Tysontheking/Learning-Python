e8d7d8031a2721589e65a1c12d58688872a0b410
Satyam <satyam10253@gmail.com>
2026-09-09 23:44:46 +0530

    api in flask

---
+++ Learn_Flask/api in flask/main.py
@@ -0,0 +1,17 @@
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    marks = {
+        "Maths": 90,
+        "Science": 85,
+        "English": 88,
+        "History": 92
+    }
+    values = [12, 34, 56, 78,marks]
+    return jsonify(values)
+
+
+app.run(debug=True,port=8000)
