790b2a26ec4dc879d864b43adc4f1b3dfc54e9c3
Satyam <satyam10253@gmail.com>
2026-09-09 23:33:47 +0530

    query parameters

---
+++ Learn_Flask/query parameters in flask/main.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    name = "satyam"
+    token = 3600
+    if "name" in request.args.keys():
+        name = request.args["name"]
+    if "token" in request.args.keys():
+        token = request.args["token"]
+    
+    return render_template("index.html", name=name,token=token)
+
+app.run(debug=True,port=8000)
